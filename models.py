18bddc04c35f53bf9575b87c52c4db0c38a59333
jf <jpf0628@gmail.com>
2022-08-02 11:59:32 -0700

    testing

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from email import message
 
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import create_access_token
+# from flask_jwt_extended import create_access_token
 from sqlalchemy import false
 
 DEFAULT_IMAGE_URL = "https://st.depositphotos.com/1835047/1677/i/600/depositphotos_16770723-stock-photo-tree-house.jpg"
@@ -115,41 +115,41 @@ class User(db.Model):
             image_url=image_url,
         )
 
-        token = create_access_token(identity=username)
+        # token = create_access_token(identity=username)
 
         db.session.add(user)
-        return token
+        # return token
 
-    @classmethod
-    def authenticate(cls, username, password):
-        """Find user with `username` and `password`.
+    # @classmethod
+    # def authenticate(cls, username, password):
+    #     """Find user with `username` and `password`.
 
-        It searches for a user whose password hash matches this password
-        and, if it finds such a user, returns that user object.
+    #     It searches for a user whose password hash matches this password
+    #     and, if it finds such a user, returns that user object.
 
-        If can't find matching user (or if password is wrong), returns False.
-        """
+    #     If can't find matching user (or if password is wrong), returns False.
+    #     """
 
-        user = cls.query.filter_by(username=username).first()
+    #     user = cls.query.filter_by(username=username).first()
 
-        if user:
-            is_auth = bcrypt.check_password_hash(user.password, password)
-            if is_auth:
-                token = create_access_token(identity=username)
-                return token
+    #     if user:
+    #         is_auth = bcrypt.check_password_hash(user.password, password)
+    #         if is_auth:
+    #             token = create_access_token(identity=username)
+    #             return token
 
-        return False
+    #     return False
 
-    def serialize(self):
-        """Serialize to dictionary."""
+    # def serialize(self):
+    #     """Serialize to dictionary."""
 
-        return {
-            "username":self.username,
-            "firstName":self.first_name,
-            "lastName":self.last_name,
-            "email":self.email,
-            "imageUrl":self.image_url,
-        }
+    #     return {
+    #         "username":self.username,
+    #         "firstName":self.first_name,
+    #         "lastName":self.last_name,
+    #         "email":self.email,
+    #         "imageUrl":self.image_url,
+    #     }
 
 
 class Message(db.Model):
