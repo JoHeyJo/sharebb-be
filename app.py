dea93b62afb328bea05395d3f2151426d3704ed6
jf <jpf0628@gmail.com>
2023-03-28 11:58:25 -0700

    removed database check

--- app.py
+++ app.py
@@ -32,15 +32,6 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 toolbar = DebugToolbarExtension(app)
 jwt = JWTManager(app)
 
-engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-inspector = sa.inspect(engine)
-if not inspector.has_table("users"):
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-        app.logger.info('Initialized the database!')
-else:
-    app.logger.info('Database already contains the users table.')
 
 connect_db(app)
 
